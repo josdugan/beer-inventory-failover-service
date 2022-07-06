0177b2e76d6318fa86f68779ac0174764e7fc562
josdugan <josdugan@gmail.com>
2022-07-06 14:03:19 -0400

    Initial commit

---
+++ src/main/java/com/josdugan/beerinventoryfailoverservice/BeerInventoryFailoverServiceApplication.java
@@ -0,0 +1,13 @@
+package com.josdugan.beerinventoryfailoverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerInventoryFailoverServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerInventoryFailoverServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josdugan/beerinventoryfailoverservice/BeerInventoryFailoverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josdugan.beerinventoryfailoverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerInventoryFailoverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
